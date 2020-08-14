7b34013c1a400586ad4de54762cb01767620c41e
vaneycksr <vaneyck23@gmail.com>
2020-08-13 23:07:19 -0300

    adicionando dependencias necessarias no pom e criando a classe base de configuracao dos endpoints

---
+++ src/test/java/support/BaseTest.java
@@ -0,0 +1,36 @@
+package support;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+
+public class BaseTest {
+
+    @BeforeClass
+    public static void setUp(){
+
+        // habilita os logs da requisição
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        // uri padrão para todos os endpoints
+        baseURI = "http://demo.testfire.net/";
+
+        // path padrão
+        basePath = "api/";
+
+        // define que todas as requisições que serão enviadas vão ser do tipo JSON
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
+
+        // define que todas respostas das requisições vão ser do tipo JSON
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+}
